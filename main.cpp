64a62bb910cfd6691bbd923e64e2d512cb03bee3
uncooljustine <j.orawska@gmail.com>
2016-11-21 22:37:14 +0100

    zmiana 3

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@
 
 using namespace std;
 
-vector<vector<char> > tabula_recta_duze()
+
+vector<vector<char> > tabula_recta()
 {
 	const int N = 26;
 	vector<vector<char> > tabula(N, vector<char>(N));
@@ -23,52 +24,25 @@ vector<vector<char> > tabula_recta_duze()
 		pozycja = 0;
 		for (kolumna = 0; kolumna < N; kolumna++)
 		{
-			if ('A' + rzad + kolumna > 'Z' && rzad > 0)
+			if ('a' + rzad + kolumna > 'z' && rzad > 0)
 			{
-				tabula[rzad][kolumna] = 'A' + pozycja;
+				tabula[rzad][kolumna] = 'a' + pozycja;
 				pozycja++;
 			}
 
-			else if ('A' + rzad + kolumna == 'Z')
+			else if ('a' + rzad + kolumna == 'z')
 			{
-				tabula[rzad][kolumna] = 'Z';
+				tabula[rzad][kolumna] = 'z';
 			}
 
 			else
 			{
-				tabula[rzad][kolumna] = 'A' + rzad + kolumna;
+				tabula[rzad][kolumna] = 'a' + rzad + kolumna;
 			}
 		}
 	}
 	return tabula;
 }
-
-vector<vector<char> > tabula_recta_male (vector<vector<char> > tabula_recta_duze())
-{
-	vector<vector <char> > tabula = tabula_recta_duze();                //przeksztalcanie tablicy z duzymi literami na druga, z malymi
-	const int N = 26;
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            tabula[i][j] = tolower(tabula[i][j]);
-        }
-    }
-
-
- for (int i = 0; i < N; i++) //debug
-    {
-        for (int j = 0; j < N; j++)
-        {
-           cout <<  tabula[i][j] <<" ";
-        }
-        cout << endl;
-    }
-
-	return tabula;
-}
-
 
 string odczytanie(string nazwa_wejscie)                         //funkcja odczytuje  dane z pliku
 {
@@ -81,13 +55,11 @@ string odczytanie(string nazwa_wejscie)                         //funkcja odczyt
 		return "blad";
 	}
 
-	else
-	{
-		while (!plik.eof())
-		{
-			getline(plik, wejscie);
-		}
-	}
+
+    while (getline(plik, wejscie))
+    {        getline(plik, wejscie);
+    }
+
 	plik.close();
 	return wejscie;
 }
@@ -95,22 +67,12 @@ string odczytanie(string nazwa_wejscie)                         //funkcja odczyt
 string szyfrowanie(string nazwa_wejscie, string klucz)
 {
 	string tekst = odczytanie(nazwa_wejscie);
+	transform(tekst.begin(), tekst.end(), tekst.begin(), ::tolower);
 	string zaszyfrowany = "";
-	vector<vector<char> > tabula_duze = tabula_recta_duze();
-  	vector<vector<char> > tabula_male = tabula_recta_male(vector<vector<char> > tabula_recta_duze());
+	vector<vector<char> > tabula = tabula_recta();
 
+    cout << "tekst do szyfrowania: " << tekst<<endl;                            //debug
 	int dlugosc = tekst.length();
-	string polskieZnaki = "ąćęłóśńżźĄĆĘŁÓŚŃŻŹ";
-	string odpowiedniki = "acelosnzzACELOSNZZ";
-	int znak = 0;
-	for (int i = 0; i < dlugosc; i++)
-	{
-		if (tekst[i] == polskieZnaki[znak])
-		{
-			tekst[i] = odpowiedniki[znak];
-			znak++;
-		}
-	}
 	int iterator = 0;
 	while (klucz.length() < dlugosc)                 // sprawdzam czy klucz jest wystarczajaco dlugi
 	{                                                // jezeli nie to powtarzam go tak dlugo, az bedzie minimum dlugosci tekstu
@@ -119,50 +81,36 @@ string szyfrowanie(string nazwa_wejscie, string klucz)
 	}
 
 	transform(klucz.begin(), klucz.end(), klucz.begin(), ::tolower);
-	int rozmiar = tabula_duze[0].size();            //rozmiar taki sam jest dla tabuli_duze jak i tabuli_male
+	int rozmiar = tabula[0].size();
 	int a, b;
 
 
 
 	for (int i = 0; i < dlugosc; i++)
 	{
-		if (isupper(tekst[i]))
+		if (isalpha(tekst[i]))
 		{
 			for (int j = 0; j < rozmiar; j++)
 			{
-				if (tabula_duze[0][j] == tekst[i])
+				if (tabula[0][j] == tekst[i])       //sprawdzam gdzie w wierszu znajduje sie litera tekstu
 				{
 					a = j;
 				}
-				if (tabula_male[j][0] == klucz[i])
+				if (tabula[j][0] == klucz[i])       //sprawdzam gdzie w kolumnie znajduje sie  litera klucza
 				{
 					b = j;
 				}
 			}
-			zaszyfrowany += tabula_duze[a][b];
+			zaszyfrowany += tabula[a][b];
 		}
 
-		else if (islower(tekst[i]))
-		{
-			for (int j = 0; j < rozmiar; j++)
-			{
-				if (tabula_male[0][j] == tekst[i])
-				{
-					a = j;
-				}
-				if (tabula_male[j][0] == klucz[i])
-				{
-					b = j;
-				}
-			}
-			zaszyfrowany += tabula_male[a][b];
-		}
+
 		else
 		{
-			tekst[i] = tekst[i];
+			zaszyfrowany += tekst[i];
 		}
 	}
-
+cout << "tekst zaszyfrowany: " << zaszyfrowany<<endl;       //debug
 	return zaszyfrowany;
 }
 
@@ -191,7 +139,7 @@ void pomoc()
 		<< "Aby podac nazwe pliku, ktory ma zostac zaszyfrowany wpisz przelacznik -i a nastepnie podaj nazwe (np. szyfr.txt)" << endl
 		<< "Aby podac nazwe pliku, w ktorym zapisany zostanie zaszyfrowany tekst wpisz przelacznik -o a nastepnie podaj nazwe (np. zaszyfrowane.txt)" << endl
 		<< "Aby uzyskac pomoc wpisz przelacznik -h" << endl
-		<< "Pamietaj! Szyfrowane sa tylko male oraz duze litery, wszelkie polskie znaki zostaja zastapione odpowiednikami. Liczby, znaki interpunkcyjne oraz znaki biale nie sa szyfrowane a jedynie przepisane w tekscie!" << endl
+		<< "Pamietaj! Program nie szyfruje polskich znakow, liczb oraz znakow interpunkcyjnych!" << endl
 		<< "Przyklad poprawnego wywolania programu: vigenere.exe -i wejscie.txt -o wyjscie.txt -k klucz" << endl;
 }
 
@@ -202,6 +150,7 @@ int main(int argc, char* argv[])
     if (argc < 2 || argc > 8)                                          //sprawdzenie, czy uzytkownik nie podaje za malo lub za duzo argumentow
 	{
 		cout << "Podano niewlasciwa ilosc argumentow" << endl << endl;
+		pomoc();
 		return 0;
 	}
 
@@ -214,6 +163,7 @@ int main(int argc, char* argv[])
 			string arg = argv[i];
 			if (arg == "-h")
 			{
+			    pomoc();
 				return 0;
 			}
 
@@ -234,6 +184,7 @@ int main(int argc, char* argv[])
 		}
 	}
 
+
        if (odczytanie(nazwa_wejscie) == "blad")
         {
             cerr << "Nie udalo sie otworzyc pliku do zaszyfrowania";
@@ -247,6 +198,11 @@ int main(int argc, char* argv[])
         {
             cerr << "Nie udalo sie zapisac pliku" << endl;
             exit(EXIT_FAILURE);
+        }
+
+        else
+        {
+            cout << "Tekst zostal zaszyfrowany oraz zapisany do pliku!" << endl;
         }
 
 
